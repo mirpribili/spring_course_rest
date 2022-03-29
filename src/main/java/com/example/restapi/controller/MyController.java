4f0aaedc56d6e361fb2d5b0ae297aad2f3480fa2
Mirpribili <mirpribili@ya.ru>
2022-03-29 16:09:06 +0300

    Изменение существующего

--- src/main/java/com/example/restapi/controller/MyController.java
+++ src/main/java/com/example/restapi/controller/MyController.java
@@ -35,4 +35,10 @@ public class MyController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
